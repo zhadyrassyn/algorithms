1f789459fd9ef0563082ed3a0e85979913257e5d
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-10-17 09:50:35 +0600

    array list. Change from int to dynamic type

--- src/dataStructure/arrayList/MyArrayList.java
+++ src/dataStructure/arrayList/MyArrayList.java
@@ -1,4 +1,49 @@
 package dataStructure.arrayList;
 
-public class MyArrayList {
+public class MyArrayList<E> {
+
+    private int N = 2;
+    private Object[] arr = new Object[N];
+    private int index = 0;
+
+    public void add(E element) {
+        if(index + 1 == N) {
+            N *= 2;
+            Object[] temp = new Object[N];
+            System.arraycopy(arr, 0, temp, 0, arr.length);
+            arr = temp;
+        }
+        arr[index++] = element;
+    }
+
+    public E get(int i) {
+        if (i >= index) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return (E)arr[i];
+    }
+
+    public int size() {
+        return arr.length;
+    }
+
+    public void print() {
+        int i = 0;
+        while(i < index) {
+            System.out.print(arr[i] + ",");
+            ++i;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(2);
+        myArrayList.add(4);
+        myArrayList.add(8);
+        myArrayList.print();
+
+        System.out.println(myArrayList.get(2));
+//        System.out.println(myArrayList.get(3));
+    }
 }
